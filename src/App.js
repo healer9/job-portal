088a93ffcfa55790584e5632ed8a6fbfac526ffd
ANAS RIYAZ <anasriyaz92@gmail.com>
2021-11-22 00:06:02 +0530

    Url Fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './routes/Home'
 import ShortListed from './routes/ShortListed'
 import Rejected from './routes/Rejected'
